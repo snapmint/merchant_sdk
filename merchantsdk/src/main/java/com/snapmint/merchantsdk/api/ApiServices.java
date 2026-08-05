2bfaabb6ccdae8285fddca2eb6d7bc49ff01a749
Maharsh <maharsh.patel@snapmint.com>
2026-08-05 15:19:32 +0530

    removed non needed things

--- merchantsdk/src/main/java/com/snapmint/merchantsdk/api/ApiServices.java
+++ merchantsdk/src/main/java/com/snapmint/merchantsdk/api/ApiServices.java
@@ -3,12 +3,8 @@ package com.snapmint.merchantsdk.api;
 
 import com.snapmint.merchantsdk.models.EmiModel;
 
-import java.util.Map;
-
 import retrofit2.Call;
-import retrofit2.http.Body;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ApiServices {
@@ -16,6 +12,4 @@ public interface ApiServices {
     @GET("assets/merchant/{path}")
     Call<EmiModel> getMerchantDetail(@Path("path") String path);
 
-    @POST("api/logs")
-    Call<Object> logMessage(@Body Map<String, String> payload);
 }
--- merchantsdk/src/main/java/com/snapmint/merchantsdk/constants/SnapmintConfiguration.java
+++ merchantsdk/src/main/java/com/snapmint/merchantsdk/constants/SnapmintConfiguration.java
@@ -1,13 +1,8 @@
 package com.snapmint.merchantsdk.constants;
 
-public class SnapmintConfiguration {
-    public static final String QA = "qa";
-    public static final String TITAN_QA = "titan_qa";
-    public static final String PRE = "pre";
-    public static final String PROD = "prod";
+public class SnapmintConfiguration { ;
     public static String STATUS = "status";
     public static String SUCCESS = "success";
     public static String FAILED = "Failed";
     public static String DATA = "data";
-    public static final int SNAPMINT_PAYMENT = 101;
 }
